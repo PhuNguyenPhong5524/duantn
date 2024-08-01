751f31d6f299e9d3c54f8c4abf190a7f66fe8f17
nguyenphongphu1234 <134608320+PhuNguyenPhong5524@users.noreply.github.com>
2024-08-01 20:27:45 +0700

    aaaaaaa

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-orange-400 p">
+      <h1 className="text-4xl font- text-yellow-50 m-5 ">Hello</h1>
+    </div>
+  );
+}
+
+export default App;
